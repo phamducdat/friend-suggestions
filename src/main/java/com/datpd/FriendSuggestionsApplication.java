cd01bd6fbeacc6faf45b41b77a43668b4da6be86
phamducdat <phamducdat2402@gmail.com>
2023-10-22 01:03:33 +0700

    init

---
+++ src/main/java/com/datpd/FriendSuggestionsApplication.java
@@ -0,0 +1,13 @@
+package com.datpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendSuggestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendSuggestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datpd/FriendSuggestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendSuggestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
